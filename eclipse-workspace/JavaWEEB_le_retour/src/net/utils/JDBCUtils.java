460fd7b0610a06740278a7197af7d67b821dab65
Alzaphano <mathieu.laumonnier@gmail.com>
2020-03-20 10:04:57 +0100

    url bdd

--- eclipse-workspace/JavaWEEB_le_retour/src/net/utils/JDBCUtils.java
+++ eclipse-workspace/JavaWEEB_le_retour/src/net/utils/JDBCUtils.java
@@ -9,9 +9,9 @@ import java.time.LocalDate;
 
 public class JDBCUtils {
 
-	private static String jdbcURL = "jdbc:mysql://localhost:8080/demo";
-	private static String jdbcUsername = "root";
-	private static String jdbcPassword = "root";
+	private static String jdbcURL = "jdbc:mysql://vps.tonychouteau.fr";
+	private static String jdbcUsername = "JavaWEEB";
+	private static String jdbcPassword = "JavaWEEB";
 
 	public static Connection getConnection() {
 		Connection connection = null;
