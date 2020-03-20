e2fd09a52beb56a7bd34055d3aa6e47f76af954e
ElisabethB <ebarre@enssat.fr>
2020-03-20 10:02:46 +0100

    Quick fix

--- eclipse-workspace/JavaWEEB_le_retour/src/net/dao/UserDao.java
+++ eclipse-workspace/JavaWEEB_le_retour/src/net/dao/UserDao.java
@@ -10,7 +10,7 @@ import net.utils.JDBCUtils;
 
 public class UserDao {
 
-	public int registerEmployee(User utilisateur) throws ClassNotFoundException {
+	public int registerEmployee(User utilisateur) throws ClassNotFoundException, SQLException {
 		String INSERT_USERS_SQL = "INSERT INTO Utilisateurs"
 				+ "  (nom, prenom, dateDeNaissance, email, dateInscription) VALUES "
 				+ " (?, ?, ?, ?, ?);";
@@ -28,27 +28,28 @@ public class UserDao {
 			System.out.println(preparedStatement);
 			// Step 3: Execute the query or update query
 			result = preparedStatement.executeUpdate();
-			
-		String INSERT_Account_SQL = "INSERT INTO Compte"
-				+ "  (identifiant, modDePasse) VALUES "
-				+ " (?, ?);";
 
-		int result1 = 0;
-		try (Connection connection1 = JDBCUtils.getConnection();
-				// Step 2:Create a statement using connection object
-				PreparedStatement preparedStatement1 = connection.prepareStatement(INSERT_Account_SQL)) {
-			preparedStatement.setString(1, utilisateur.getIdentifiant());
-			preparedStatement.setString(2, utilisateur.getMotDePasse());
+			String INSERT_Account_SQL = "INSERT INTO Compte"
+					+ "  (identifiant, modDePasse) VALUES "
+					+ " (?, ?);";
 
-			System.out.println(preparedStatement1);
-			// Step 3: Execute the query or update query
-			result1 = preparedStatement1.executeUpdate();
+			int result1 = 0;
+			try (Connection connection1 = JDBCUtils.getConnection();
+					// Step 2:Create a statement using connection object
+					PreparedStatement preparedStatement1 = connection.prepareStatement(INSERT_Account_SQL)) {
+				preparedStatement.setString(1, utilisateur.getIdentifiant());
+				preparedStatement.setString(2, utilisateur.getMotDePasse());
 
-		} catch (SQLException e) {
-			// process sql exception
-			JDBCUtils.printSQLException(e);
+				System.out.println(preparedStatement1);
+				// Step 3: Execute the query or update query
+				result1 = preparedStatement1.executeUpdate();
+
+			} catch (SQLException e) {
+				// process sql exception
+				JDBCUtils.printSQLException(e);
+			}
+			return result;
 		}
-		return result;
 	}
 
 }
