c357b070c6bcc1f4c2a397f4e0ae572a52e7bbfe
Philippe MARTIN <pmartin@enssat.fr>
2020-03-22 13:55:34 +0100

    pt sauv.

--- eclipse-workspace/JavaWEEB_le_retour/src/net/web/BibliothequeJeuxController.java
+++ eclipse-workspace/JavaWEEB_le_retour/src/net/web/BibliothequeJeuxController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -13,6 +14,7 @@ import net.dao.JeuService;
 import net.dao.JeuServiceImpl;
 import net.model.Jeu;
 
+@WebServlet("/admin")
 public class BibliothequeJeuxController extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
