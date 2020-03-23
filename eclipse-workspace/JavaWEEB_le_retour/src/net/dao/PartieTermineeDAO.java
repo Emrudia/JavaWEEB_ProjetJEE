6141f822828abe6ebf71f0755a09f4e222287131
Alzaphano <mathieu.laumonnier@gmail.com>
2020-03-23 17:08:21 +0100

    Date partie terminee

--- eclipse-workspace/JavaWEEB_le_retour/src/net/dao/PartieTermineeDAO.java
+++ eclipse-workspace/JavaWEEB_le_retour/src/net/dao/PartieTermineeDAO.java
@@ -4,6 +4,8 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,18 +14,19 @@ import net.utils.JDBCUtils;
 
 public class PartieTermineeDAO {
 	
-	public List<PartieTerminee> findByAll() throws InstantiationException, IllegalAccessException{
+	public List<PartieTerminee> findByAll() throws InstantiationException, IllegalAccessException, ParseException{
 		Connection connexion = null;
 		Statement statement = null;
 		ResultSet rs = null;
 		List<PartieTerminee> list = new ArrayList<PartieTerminee>();
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy hh:mm:ss");
 		
 		try{
 			connexion = JDBCUtils.getConnection();
 			statement = connexion.createStatement();
 			rs = statement.executeQuery("select * from PartieTerminee;") ;
 			while (rs.next()) {				
-				PartieTerminee partie = new PartieTerminee(rs.getInt("idUtilisateur"),rs.getInt("idJeu"),rs.getString("nomJeu"),JDBCUtils.getUtilDate(rs.getDate("dateDebut")),JDBCUtils.getUtilDate(rs.getDate("dateFin")));
+				PartieTerminee partie = new PartieTerminee(rs.getInt("idUtilisateur"),rs.getInt("idJeu"),rs.getString("nomJeu"), sdf.parse(rs.getString("dateDebut")),sdf.parse(rs.getString("dateFin")));
 				list.add(partie);
 			}
 			
