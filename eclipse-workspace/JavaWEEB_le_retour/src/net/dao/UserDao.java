f70c1eaed23e7b0e53ec530a779debfab730e5f4
Alzaphano <mathieu.laumonnier@gmail.com>
2020-03-23 15:34:55 +0100

    fix acces bdd

--- eclipse-workspace/JavaWEEB_le_retour/src/net/dao/UserDao.java
+++ eclipse-workspace/JavaWEEB_le_retour/src/net/dao/UserDao.java
@@ -12,39 +12,40 @@ public class UserDao {
 
 	public int registerEmployee(User utilisateur) throws ClassNotFoundException, SQLException, InstantiationException, IllegalAccessException {
 		String INSERT_USERS_SQL = "INSERT INTO Utilisateur"
-				+ "  (nom, prenom, dateDeNaissance, email, dateInscription) VALUES "
-				+ " (?, ?, ?, ?, ?);";
+				+ "  (nom, prenom, dateDeNaissance, email, dateInscription, Compte_identifiant) VALUES "
+				+ " (?, ?, ?, ?, ?, ?);";
 		
 		String INSERT_Account_SQL = "INSERT INTO Compte"
-				+ "  (identifiant, modDePasse) VALUES "
+				+ "  (identifiant, motDePasse) VALUES "
 				+ " (?, ?);";
 		
 		int result = 0;
-		try (Connection connection = JDBCUtils.getConnection();
+		try (Connection connection1 = JDBCUtils.getConnection();
 				// Step 2:Create a statement using connection object
-			PreparedStatement preparedStatement = connection.prepareStatement(INSERT_USERS_SQL)) {
-			preparedStatement.setString(1, utilisateur.getNom());
-			preparedStatement.setString(2, utilisateur.getPrenom());
-			preparedStatement.setDate(3, JDBCUtils.getSQLDate(utilisateur.getDateNaissance()));
-			preparedStatement.setString(4, utilisateur.getEmail());
-			preparedStatement.setDate(5, JDBCUtils.getSQLDate(LocalDate.now()));
+				PreparedStatement preparedStatement1 = connection1.prepareStatement(INSERT_Account_SQL)) {
+			preparedStatement1.setString(1, utilisateur.getIdentifiant());
+			preparedStatement1.setString(2, utilisateur.getMotDePasse());
 
-			System.out.println(preparedStatement);
+			System.out.println(preparedStatement1);
 			// Step 3: Execute the query or update query
-			result = preparedStatement.executeUpdate();
-
-			
+			result= preparedStatement1.executeUpdate();
+				
+				
+		
 
 			int result1 = 0;
-			try (Connection connection1 = JDBCUtils.getConnection();
+			try (Connection connection = JDBCUtils.getConnection();
 					// Step 2:Create a statement using connection object
-					PreparedStatement preparedStatement1 = connection.prepareStatement(INSERT_Account_SQL)) {
-				preparedStatement1.setString(1, utilisateur.getIdentifiant());
-				preparedStatement1.setString(2, utilisateur.getMotDePasse());
-
-				System.out.println(preparedStatement);
-				// Step 3: Execute the query or update query
-				result1 = preparedStatement1.executeUpdate();
+					PreparedStatement preparedStatement = connection.prepareStatement(INSERT_USERS_SQL)) {
+					preparedStatement.setString(1, utilisateur.getNom());
+					preparedStatement.setString(2, utilisateur.getPrenom());
+					preparedStatement.setDate(3, JDBCUtils.getSQLDate(utilisateur.getDateNaissance()));
+					preparedStatement.setString(4, utilisateur.getEmail());
+					preparedStatement.setDate(5, JDBCUtils.getSQLDate(LocalDate.now()));
+					preparedStatement.setString(6, utilisateur.getIdentifiant());
+					System.out.println(preparedStatement);
+					// Step 3: Execute the query or update query
+					result1 = preparedStatement.executeUpdate();
 
 			} catch (SQLException e) {
 				// process sql exception
