37fbcdf15ffbcd809f8e8112987bc6ecab13750d
ElisabethB <ebarre@enssat.fr>
2020-03-23 16:15:15 +0100

    Partie Terminee

--- eclipse-workspace/JavaWEEB_le_retour/src/net/model/PartieTerminee.java
+++ eclipse-workspace/JavaWEEB_le_retour/src/net/model/PartieTerminee.java
@@ -6,16 +6,27 @@ public class PartieTerminee {
 	
 	private int idUtilisateur;
 	private int idJeu;
+	private String nom;
 	private LocalDate dateDebut;
 	private LocalDate dateFin;
 	
-	public PartieTerminee(int idUtilisateur, int idJeu, LocalDate dateDebut, LocalDate dateFin) {
+	public PartieTerminee(int idUtilisateur, int idJeu,String nom, LocalDate dateDebut, LocalDate dateFin) {
 		super();
 		this.idUtilisateur = idUtilisateur;
 		this.idJeu = idJeu;
+		this.nom=nom;
 		this.dateDebut = dateDebut;
 		this.dateFin = dateFin;
 	}
+	
+	public String getNom() {
+		return nom;
+	}
+	
+	public void setNom(String nom) {
+		this.nom=nom;
+	}
+
 
 	public int getIdUtilisateur() {
 		return idUtilisateur;
