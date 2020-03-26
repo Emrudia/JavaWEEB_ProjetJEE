e24b6a8001f1e55e3e251c467f34522e3d8fe452
hbernat <hbernat@enssat.fr>
2020-03-26 02:45:18 +0100

    Auto stash before merge of "master" and "origin/master"

--- eclipse-workspace/JavaWEEB_le_retour/src/net/web/UpdateJeuxFavorisController.java
+++ eclipse-workspace/JavaWEEB_le_retour/src/net/web/UpdateJeuxFavorisController.java
@@ -16,7 +16,7 @@ import net.dao.TodoDaoImpl;
 import net.model.Jeu;
 import net.model.User;
 
-@WebServlet(urlPatterns = {"/delete"}) 
+@WebServlet(urlPatterns = {"/deleteJeu", "/addJeu"}) 
 
 public class UpdateJeuxFavorisController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -40,9 +40,11 @@ public class UpdateJeuxFavorisController extends HttpServlet {
 			System.out.println("context Path :" + request.getContextPath());
 			switch (action) {
 
-			case "/delete":
+			case "/deleteJeu":
 				deleteJeuFavori(request, response);
 				break;
+			case "/addJeu":
+				addJeuFavori(request,response);
 
 			default:
 				response.sendRedirect("JSP/accueil_nonlog.jsp");
@@ -71,6 +73,22 @@ public class UpdateJeuxFavorisController extends HttpServlet {
 		request.getSession().setAttribute("sessionUtilisateur",user);
 		
 		response.sendRedirect("JSP/profil_user.jsp");
+	}
+	
+	private void addJeuFavori(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+		String jeu = request.getParameter("jeu");
+		String[] NomId = jeu.split("XXX");
+		Jeu jeuFavori = new Jeu();
+		jeuFavori.setIdJeu(Integer.parseInt(NomId[1]));
+		jeuFavori.setNom(NomId[0]);
+
+		User user = (User) request.getSession().getAttribute("sessionUtilisateur");
+		user.getJeuxFavoris().add(jeuFavori);
+		request.getSession().setAttribute("sessionUtilisateur",user);
+		jeuDAO.addJeuFavori(Integer.parseInt(NomId[1]),user.getIdUtilisateur());
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher("JSP/games_user.jsp");
+		dispatcher.forward(request, response);
 	}
 	
 }
