e8d0a833899c8ea574f830a5949e0590978fa7ba
ElisabethB <ebarre@enssat.fr>
2020-03-26 15:23:57 +0100

    PartieTermineeDAO

--- eclipse-workspace/JavaWEEB_le_retour/src/net/dao/PartieTermineeDAO.java
+++ eclipse-workspace/JavaWEEB_le_retour/src/net/dao/PartieTermineeDAO.java
@@ -1,10 +1,9 @@
 package net.dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.text.ParseException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
@@ -15,26 +14,38 @@ import net.utils.JDBCUtils;
 
 public class PartieTermineeDAO {
 	
-	public static List<PartieTerminee> findByAll() throws InstantiationException, IllegalAccessException, ParseException{
-		Connection connexion = null;
-		Statement statement = null;
-		ResultSet rs = null;
-		List<PartieTerminee> list = new ArrayList<PartieTerminee>();
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy hh:mm:ss");
+	public static List<PartieTerminee> findByAll(){
 		
-		try{
+		Connection connexion;
+		List<PartieTerminee> list = new ArrayList<PartieTerminee>();
+		try {
 			connexion = JDBCUtils.getConnection();
-			statement = connexion.createStatement();
-			rs = statement.executeQuery("select * from PartieTerminee;") ;
-			while (rs.next()) {				
-				PartieTerminee partie = new PartieTerminee(rs.getInt("idUtilisateur"),rs.getInt("idJeu"),rs.getString("nomUtilisateur"), rs.getString("nomJeu"), LocalDateTime.parse(rs.getString("dateDebut"), formatter), LocalDateTime.parse(rs.getString("dateFin"), formatter));
-				list.add(partie);
-			}
+			String request = "select * from PartieTerminee;";
+			PreparedStatement preparedStatement = connexion.prepareStatement(request);
+			ResultSet rs = preparedStatement.executeQuery();
 			
+			DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy hh:mm:ss");
+			
+			while (rs.next()) {
+				list.add(new  PartieTerminee(rs.getInt("idUtilisateur"),
+											 rs.getInt("idJeu"),
+											 rs.getString("nomUtilisateur"), 
+											 rs.getString("nomJeu"), 
+											 LocalDateTime.parse(rs.getString("dateDebut"), formatter), 
+											 LocalDateTime.parse(rs.getString("dateFin"), formatter))
+				);
+			}
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
 		return list;
 	}
 
